ef33ac2b8c8b04fc1e1ed70c66d95d61c1110197
Sankara Sundararaman <ssundar@redhat.com>
2023-06-06 17:17:15 -0500

    Removed unused methods from code

--- rhsfUtils.py
+++ rhsfUtils.py
@@ -11,16 +11,11 @@
 
 # Import required modules
 from flask import jsonify,Response
-import tarfile
-import urllib.request
 import os
 from subprocess import call
 from subprocess import Popen, PIPE
 import subprocess
-import sys
 import logging
-import platform
-
 try:
     import simplejson as json
 except ImportError:
@@ -28,153 +23,32 @@ except ImportError:
 
 def rhsf_init():
     # Base directories
-    global home_dir, downloads_dir,download_sfdx_file,sf_dir,cwd
+    global sf_dir
 
-    home_dir = os.getenv("HOME")
-    downloads_dir = (home_dir+"/Downloads")
-    download_sfdx_file = (downloads_dir+"/sfdx-linux-amd64.tar.xz")
-    sf_dir = (home_dir+"/salesforce")
-    cwd = os.getcwd()
+    home_dir = os.getenv("HOME")    
+    sf_dir = (home_dir+"/salesforce")   
+    print("Checking if "+sf_dir+" exists") 
 
     # Ensure salesforce cli is installed and user is authenticated
-    salesforce_prereq(downloads_dir, download_sfdx_file, sf_dir)
-
-def salesforce_prereq(downloads_dir, download_sfdx_file, sf_dir):
-    """ Ensure sfdx is installed and that the user is authenticated """
-    dev_null = open(os.devnull, 'wb')
-    # Download and install the sfdx cli, if it's not already installed
-    return_code = subprocess.call(['which', 'sfdx'], stdout=dev_null, stderr=subprocess.STDOUT)
-    detected_os = platform.system()
-    if return_code != 0:
-        print("You do not have the salesforce CLI installed.")
-        print("Starting install, this will take a couple of minutes.")
-
-        # If CLI is not yet downloaded, download tar archive
-        if not os.path.isfile(download_sfdx_file):
-            if detected_os == "Darwin":
-                print("You are on MacOS. Install the Salesforce CLI from:\
-                       https://developer.salesforce.com/tools/sfdxcli#")
-                sys.exit(1)
-
-            dl_url = 'https://developer.salesforce.com/media/salesforce-cli/sfdx/channels/stable/sfdx-linux-x64.tar.gz'
-            urllib.request.urlretrieve(dl_url, download_sfdx_file)
-
-        # Extract tar archive to ~/Downloads
-        tar = tarfile.open(download_sfdx_file)
-        tar.extractall(path=downloads_dir)
-        tar.close()
-
-        # Find out what the extracted directory is called
-        # it's different from the filename of the archive.
-        partial_dir = "sfdx-cli"
-        for extr_dir in os.listdir(downloads_dir):
-            if extr_dir.find(partial_dir) != -1:
-                sfdx_dir_name = extr_dir
-
-        # Call the sfdx installation script
-        full_sfdx_dir_name = (downloads_dir+"/"+sfdx_dir_name)
-        sfdx_call = ("sudo "+full_sfdx_dir_name+"/install")
-        call(sfdx_call, cwd=full_sfdx_dir_name, shell=True)
+    #salesforce_prereq(downloads_dir, download_sfdx_file, sf_dir)
 
     # If we have not initialized a project yet, do that
     if not os.path.isdir(sf_dir):
         print("Init: Creating sfdx project.")
         call("sfdx force:project:create -n salesforce", cwd=os.getenv("HOME"), shell=True)
 
-    # Change working directory, to prevent sfdx from bailing out
+    # Change working directory, to prevent sfdx from bailing out    
     os.chdir(sf_dir)
-
-    # Check if we are authenticated, if not, authenticate
-    try:
-        output = subprocess.check_output(["sfdx", "force:auth:list"], stderr=subprocess.STDOUT)
-    except subprocess.CalledProcessError:
-        output = ""
-
-    if output == "" or b'redhat.com' not in output:
-        print("Init: We are not authenticated. Please login via the opened web page.")
-        call("sfdx force:auth:web:login --instance-url https://redhat.my.salesforce.com\
-              --alias redhat", cwd=sf_dir, shell=True)
-        call("sfdx config:set defaultusername=redhat", cwd=sf_dir, shell=True)        
-    return
-
-def find_forecast_group(product_line):
-    if not product_line:
-        return ""
-    """ Map products with forecast groups """
-
-    # Define lists with products per group.
-    # Taken from our Master Product Hierachy
-    openshift = ['OpenShift Dedicated',\
-                 'OpenShift Enterprise',\
-                 'OpenShift Online',\
-                 'OpenShift TAM',\
-                 'Cloud Suite']
-    cloud_infrastructure = ['Ansible',\
-                            'OpenStack',\
-                            'Platform Fees',\
-                            'Platform TAM',\
-                            'RHCI',\
-                            'RHCI TAM',\
-                            'RHEV',\
-                            'Virtualization TAM']
-    middleware = ['3scale',\
-                  'A-MQ',\
-                  'BPM Suite',\
-                  'BRMS',\
-                  'Data Grid',\
-                  'Data Virtualization',\
-                  'Developer Support',\
-                  'Enterprise Application Platform',\
-                  'Enterprise Portal Platform',\
-                  'Enterprise Web Platform',\
-                  'Fuse',\
-                  'Fuse Service Works',\
-                  'JBoss Communication Platform',\
-                  'JBoss Developer Support',\
-                  'JBoss Web Server',\
-                  'Messaging',\
-                  'Middleware Fees',\
-                  'Middleware TAM',\
-                  'Mobile TAM',\
-                  'MRG',\
-                  'RHOAR',\
-                  'Runtime',\
-                  'SOA-P / jBPM',\
-                  '']
-    mobile = ['RHMAP']
-    rhel_satellite_csds = ['Certificate Sys and Directory Serv',\
-                           'Red Hat Insights',\
-                           'Red Hat Satellite',\
-                           'RHEL',\
-                           'RHEL w/ Smart Virtualization']
-    storage = ['Ceph Software',\
-               'Gluster Software',\
-               'Storage TAM']
-
-    if product_line in str(openshift):
-        forecast_group = "OpenShift"
-    elif product_line in str(cloud_infrastructure):
-        forecast_group = "Cloud Infrastructure"
-    elif product_line in str(middleware):
-        forecast_group = "Middleware"
-    elif product_line in str(mobile):
-        forecast_group = "Mobile"
-    elif product_line in str(rhel_satellite_csds):
-        forecast_group = "RHEL/Satellite/CS-DS"
-    elif product_line in str(storage):
-        forecast_group = "Storage"
-
-    return forecast_group
 
 def sf_get_task_by_id(task_id):
     """ Search for opportunities for a specific account id """
     logging.debug('entered search_task')
-    sfdx_call = ("sfdx force:data:soql:query -q "
+    sfdx_call = ("sfdx data:query -q "
                  "\"SELECT "
                  "Id,OwnerId,Owner.Name,ActivityDate,Status,WhatId,Subject,Description "
                  "FROM Task "
                  "WHERE Id = \'" + task_id + "\'\" "
-                 "--resultformat=json")
+                 "-r json")
     logging.debug('%s', sfdx_call)
     output = subprocess.check_output([sfdx_call], cwd=sf_dir, shell=True).decode()    
     response = Response(
